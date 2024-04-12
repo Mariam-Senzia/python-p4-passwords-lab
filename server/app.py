4a1b3e70250b3a2ba44d1c354b196d4d03f1fe8b
Mariam Senzia <mariamsenzia@gmail.com>
2024-04-13 00:48:17 +0300

    passwords

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import request, session
+from flask import request, session, jsonify
 from flask_restful import Resource
 
 from config import app, db, api
@@ -28,16 +28,42 @@ class Signup(Resource):
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    
+    def get(self):
+       if session.get('user_id'):
+           user = User.query.get('user_id')
+           return jsonify(user.to_dict()), 200
+       else:
+           return jsonify({}), 204
+       
 
 class Login(Resource):
-    pass
+    def post(self):
+
+        username = request.json.get('username')
+        password = request.json.get('password')
+
+        user = User.query.filter(User.username == username).first()
+
+        if user.authenticate(password):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+
+        return {'error': 'Invalid username or password'}, 401
+
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session['user_id'] = None
+        return {'message': '204: No Content'}, 204
+
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
